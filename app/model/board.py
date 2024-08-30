ba1be3a756b6dec73a4652d64d3404d952fa97a5
YongHeeCh <alpacatnt@naver.com>
2024-08-30 13:44:48 +0900

    sqlite: regdate 마이크로 초 0으로 설정

--- app/model/board.py
+++ app/model/board.py
@@ -15,18 +15,17 @@ class Board(Base):
     regdate: Mapped[datetime] = mapped_column(default=datetime.now)
     views: Mapped[int] = mapped_column(default=0)
     contents: Mapped[str]
-    replys = relationship('Relpy', back_populates='board')
+    replys = relationship('Reply', back_populates='board')
+
 
 class Reply(Base):
     __tablename__ = 'reply'
 
     rno: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
-    repl: Mapped[str] = mapped_column(index=True)
+    reply: Mapped[str] = mapped_column(index=True)
     userid: Mapped[str] = mapped_column(ForeignKey('member.userid'), index=True)
-    regdate: Mapped[datetime] = mapped_column(default=datetime.now)
+    # regdate: Mapped[datetime] = mapped_column(default=datetime.now)
+    regdate: Mapped[datetime] = mapped_column(default=lambda: datetime.now().replace(microsecond=0))
     bno: Mapped[int] = mapped_column(ForeignKey('board.bno'))
-    rpno: Mapped[int] = mapped_column(ForeignKey('reply.rno'), index=True)
+    rpno: Mapped[int] = mapped_column(ForeignKey('reply.rno'))
     board = relationship('Board', back_populates='replys')
-
-
-
